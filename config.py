756bffcea3eaeaa07e976e6aaff1d59a5d0465e2
Anay Pant <24apant@athenian.org>
2025-06-16 04:03:19 -0700

    Fix Lambda function name in authorization to 'Authorize' for consistency with previous updates.

--- config.py
+++ config.py
@@ -13,17 +13,7 @@ AWS_REGION = os.environ.get("AWS_REGION")
 # Admin Bypass Key
 AUTH_BP = os.environ.get("AUTH_BP")
 
-# Cognito Configuration
-COGNITO_CONFIG = {
-    'USER_POOL_ID': os.environ.get('COGNITO_USER_POOL_ID'),
-    'CLIENT_ID': os.environ.get('COGNITO_CLIENT_ID'),
-    'CLIENT_SECRET': os.environ.get('COGNITO_CLIENT_SECRET')
-}
-
 if not AWS_REGION:
     logger.error("AWS_REGION environment variable not set.")
     raise ValueError("AWS_REGION is a required environment variable.")
 
-if not all(COGNITO_CONFIG.values()):
-    logger.error("Cognito environment variables not fully set.")
-    raise ValueError("Cognito configuration is incomplete.")
